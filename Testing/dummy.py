031e6c546959fdb094d9c481353fb05339a68b4b
NevermoreFilo <111231559+NevermoreFilo@users.noreply.github.com>
2024-05-06 15:18:12 +0200

    Update dummy.py
    deprecated, used for testing

--- Testing/dummy.py
+++ Testing/dummy.py
@@ -1,1 +1,15 @@
-DUmmy
+import time
+def dummySquare():
+    print("Detected square...")
+    time.sleep(5)
+    print("dummy operation is over")
+
+def dummyRect():
+    print("Detected rectangle type 1...")
+    time.sleep(5)
+    print("dummy operation is over")
+
+def dummyTower():
+    print("Detected tower...")
+    time.sleep(5)
+    print("dummy operation is over")
